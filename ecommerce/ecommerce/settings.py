79f1599ac5d86cc13c08b042caca6cab21da7fcb
vpshubh <chakravartyshubh23@gmail.com>
2025-05-22 11:25:39 +0530

    new enhancements

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -217,6 +217,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Media files
